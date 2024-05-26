00a31fc1cbb9ef65a4b51cd52c31424d7058f789
Lunac <moon.zhp@gmail.com>
2024-05-26 20:47:42 +0800

    update run model, accept user msg

--- hg/run_model.py
+++ hg/run_model.py
@@ -2,12 +2,13 @@
 import sys
 from transformers import pipeline
 
-if len(sys.argv) < 2:
-    print("Usage: python run_model.py meta-llama/Llama-2-7b-chat-hf")
+if len(sys.argv) < 3:
+    print("Usage: python run_model.py meta-llama/Llama-2-7b-chat-hf user_msg")
     sys.exit(1)
 
 model=sys.argv[1]
+msg=sys.argv[2]
 
 pipe = pipeline("text-generation", model=model)
-results = pipe("What is the meaning of life?")
+results = pipe(msg)
 print(results)
