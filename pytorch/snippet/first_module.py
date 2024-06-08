43119ea59dd9432e10835a6d9c66454823863452
Lunac <moon.zhp@gmail.com>
2024-06-08 23:33:40 +0800

    add first model

---
+++ pytorch/snippet/first_module.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+class MLP(nn.Module):
+  # 声明带有模型参数的层，这里声明了两个全连接层
+  def __init__(self, **kwargs):
+    # 调用MLP父类Block的构造函数来进行必要的初始化。这样在构造实例时还可以指定其他函数
+    super(MLP, self).__init__(**kwargs)
+    self.hidden = nn.Linear(784, 256)
+    self.act = nn.ReLU()
+    self.output = nn.Linear(256,10)
+    
+   # 定义模型的前向计算，即如何根据输入x计算返回所需要的模型输出
+  def forward(self, x):
+    o = self.act(self.hidden(x))
+    return self.output(o)   
+
+if __name__ == '__main__':
+  X = torch.rand(2, 784)
+  net = MLP()
+  print(net)
+  print(net(X))
